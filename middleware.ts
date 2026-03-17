fb5c52ab01810fedd091d950b2c7111c87f11b75
Gustavo Herrera <gustavoh.2312@gmail.com>
2026-03-17 12:30:38 -0400

    security: 16 y 17 - HSTS/CSP headers on nextConfig y Upstash Token Rate limiting middleware añadido

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,42 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
 
-export function middleware(request: NextRequest) {
+// Inicializa redis con Upstash (requiere URl y Token como ENV, manejara fallos gracefullment si no existen)
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || 'https://dummy.upstash.io',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || 'dummy',
+});
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds (standard rate)
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(10, '10 s'),
+  analytics: true,
+});
+
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+
+  // Rate Limiting para APIs
+  if (pathname.startsWith('/api/') && process.env.UPSTASH_REDIS_REST_URL) {
+    // Identificar usuario por su IP
+    const ip = request.ip ?? '127.0.0.1';
+    const { success, limit, reset, remaining } = await ratelimit.limit(ip);
+    
+    // Si excede
+    if (!success) {
+      return new NextResponse('Too Many Requests', {
+        status: 429,
+        headers: {
+          'X-RateLimit-Limit': limit.toString(),
+          'X-RateLimit-Remaining': remaining.toString(),
+          'X-RateLimit-Reset': reset.toString(),
+        },
+      });
+    }
+  }
 
   // Rutas públicas que no requieren autenticación
   const publicPaths = ['/auth/login', '/auth/register', '/auth/forgot-password'];
